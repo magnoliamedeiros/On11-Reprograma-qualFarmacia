fc7613c779a74666a472adb34ce7a14f9e2d46ef
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-06-27 16:23:26 -0300

    Alteracao no app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const farmacias = require('./routes/')
 
 app.use(express.json())
 
+// retorna um json formatado e tabulado de forma amigável
+app.set('json spaces', 4)
+
 // definindo rota raiz
 app.use('/', index)
 app.use('/farmacias', farmacias)
