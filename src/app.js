08b12878018f237478a6c506ac850db1cb45776c
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-07-06 16:49:14 -0300

    adicionando o banco

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const index = require('./routes/index')
 const farmacia = require('./routes/farmacia.routes')
 
 // conectando o mongo ao mongoose
-// const db = require('./data/database')
-// db.connect()
+const db = require('./data/database')
+db.connect()
 
 // retorna um json formatado e tabulado de forma amigável
 app.set('json spaces', 4)
