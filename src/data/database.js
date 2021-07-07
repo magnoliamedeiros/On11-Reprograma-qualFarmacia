4d819dfd7f2dbac4166f2b7dcd31deb4b860b51a
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-07-06 22:46:13 -0300

    modificado a funcao connect

--- src/data/database.js
+++ src/data/database.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const connect = () => {mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
-.then(console.log('Database connected!'))
-.catch(err => console.log('Could not connect', err))}
+const connect = async () => {
+  try {
+      await mongoose.connect(
+          process.env.MONGO_URL,
+          {
+              useNewUrlParser: true,
+              useUnifiedTopology: true,
+              useFindAndModify: false,
+              useCreateIndex: true,
+          }
+      )
+      console.log('database connected!')
+  } catch (error) {
+      console.error(error.message)
+  }
+}
 
 module.exports = { connect }
