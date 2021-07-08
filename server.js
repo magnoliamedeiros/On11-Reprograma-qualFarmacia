507c525df16fb60a201a1c34ef2d1e81b8b9504d
magnoliamedeiros <magnoliamedeiros@gmail.com>
2021-07-08 18:01:42 -0300

    Correcao de error no server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const app = require('./src/app')
-const port = process.env.PORT
+const dotenv = require('dotenv')
+dotenv.config()
+
+const port = process.env.PORT || 3030
 
 app.listen(port, ()=>console.log(`Server is running in http://localhost:${port}`))
