1cc3521896b9204cf3570d70ff5e4588041b58a2
magnoliamedeiros <magnoliamedeiros@gmail.com>
2021-07-08 17:49:40 -0300

    Modificado o arquivo do database

--- src/data/database.js
+++ src/data/database.js
@@ -3,17 +3,13 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        })
-    console.log('Database connected!')
-    } catch (error) {
-        console.error(`Database connection error! ${error}`)
-    }
-}
+    await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+.then(console.log('Database connected!'))
+.catch(error => console.error('Could not connect', error))}
 
 module.exports = { connect }
